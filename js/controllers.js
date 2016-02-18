808ac37ae8cc217971ccde61395485e5f6ed71fa
Ian Schoonover <iantskon@gmail.com>
2016-02-18 16:09:09 -0600

    Update controller

--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,10 @@ app.controller('userCtrl', function($scope, userService) {
 	$scope.users = userService.getUsers();
 	$scope.user = {};
 	$scope.toggleEdit = false;
-
+	//example of deep watching
+	// $scope.$watch('users', function() {
+	// 	console.log('changed!');
+	// }, true);
 	$scope.addUser = function() {
 		userService.addUser($scope.user);
 		$scope.user = {};
